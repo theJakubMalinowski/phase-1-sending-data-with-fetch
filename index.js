05e40e519e026c8563a80d9bbbc0049e68dd3d06
Jakub <malinowski.jakub@yahoo.com>
2022-03-14 21:59:55 -0500

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+function submitData(userName, userEmail){
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify({
+            name : `${userName}`,
+            email: `${userEmail}`
+        })
+    }) .then(resp => resp.json())
+    .then (data => {
+        document.querySelector('body').append(data.id)})
+    .catch (error => {
+        alert("Something is very wrong.");;
+        console.log(error.message);
+        document.querySelector('body').append(error.message)
+    })
+}
